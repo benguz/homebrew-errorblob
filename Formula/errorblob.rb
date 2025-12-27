bbfd66aa2faad498dc4f1370d4564e2254a3f5e1
benguz <benjamin.guzovsky@gmail.com>
2025-12-27 12:39:30 -0800

    lol

--- Formula/errorblob.rb
+++ Formula/errorblob.rb
@@ -4,7 +4,7 @@ class Errorblob < Formula
   desc "Never block on the same bug twice - a lightning-fast error database"
   homepage "https://github.com/benguz/errorblob"
   url "https://github.com/benguz/errorblob/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3f8d07814499cc2d279586e0d05916b47513d07c2cc209dfcf9345ee079a630f"
+  sha256 "01adcdaaa683e2a585b421670ff012e231b36c55df5b5ec64563bafd17af461a"
   license "MIT"
 
   depends_on "python@3.12"
