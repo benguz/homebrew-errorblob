679a88f9ad9c7cd985374385be30bb8572f4f301
benguz <benjamin.guzovsky@gmail.com>
2025-12-27 11:48:42 -0800

    fix checksum

--- Formula/errorblob.rb
+++ Formula/errorblob.rb
@@ -4,7 +4,7 @@ class Errorblob < Formula
   desc "Never block on the same bug twice - a lightning-fast error database"
   homepage "https://github.com/benguz/errorblob"
   url "https://github.com/benguz/errorblob/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3aedfe46cfd169d467bdfcafaabec4c0c403191cbd02ee7eca7308a4c150c1be"
+  sha256 "0def86668ad919bfaa88b83b7ed7b81dc8f9272dc47bfefa8d56222ba566a19f"
   license "MIT"
 
   depends_on "python@3.12"
